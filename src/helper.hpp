7c14da6288a9ae07a909ec055aca88e8b925cee6
hakan <trh.gungor@gmail.com>
2025-11-22 16:54:23 +0100

    draft release

--- src/helper.hpp
+++ src/helper.hpp
@@ -18,35 +18,35 @@ using GuiChanges = int;
 
 /**
  * \brief Update the Graphical User Interface (GUI) and retrieve new values for the parameters.
- * 
+ *
  * All parameters are input/output, meaning their value will be used to display the GUI and
  * they will be set to the new value, as implied by user interaction with the GUI (in the previous frame).
- * 
+ *
  * \return Object that tracks changes to the parameters.
  */
 GuiChanges gui(float* azimuth, float* fov, float* size, float* znear, float* zfar, TransformationType* transformation_type);
 
 /**
  * \brief Query if an interaction with the GUI has changed a parameter value.
- * 
+ *
  * Example usage:
  * \code{.cpp}
  * int   foo = ...;
  * float bar = ...;
  *
  * GuiChanges gui_changes = gui(&foo, &bar);
- *  
+ *
  * if(has_gui_changed_parameter(gui_changes, 0))
  * {
- *     // Parameter `foo` was changed  
+ *     // Parameter `foo` was changed
  * }
- * 
+ *
  * if(has_gui_changed_parameter(gui_changes, 1))
  * {
- *     // Parameter `bar` was changed  
+ *     // Parameter `bar` was changed
  * }
  * \endcode
- * 
+ *
  * \param[in] changes         The \c GuiChanges returned by a call to \c gui(...)
  * \param[in] parameter_index The 0-based index of the parameter to query for changes
  */
@@ -54,7 +54,7 @@ bool has_gui_changed_parameter(GuiChanges changes, unsigned int parameter_index)
 
 /**
  * \brief Render a visualization of the camera defined by a view matrix and a projection matrix.
- * 
+ *
  * \param[in] renderer          The renderer which will be used
  * \param[in] view_matrix       The view matrix determining the position and orientation of the camera
  * \param[in] projection_matrix The projection matrix of the camera
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <array>
+#include <cmath>
 #include <cstdint>
 #include <iostream>
 #include <queue>
@@ -9,6 +10,7 @@
 #include <GLFW/glfw3.h>
 #include <glad/glad.h>
 #include <glm/glm.hpp>
+#include <glm/gtc/constants.hpp>
 
 #include <imgui.h>
 #include <imgui_impl_glfw.h>
@@ -31,50 +33,83 @@ int main(int argc, char** argv)
     GLFWwindow*             window     = nullptr;
     cgtub::EventDispatcher* dispatcher = nullptr;
     if (!cgtub::init(1600, 400, "CG1", &window, &dispatcher))
-    {
-        std::cerr << "Failed to initialize OpenGL window" << std::endl;
         return EXIT_FAILURE;
-    }
 
-    cgtub::Canvas canvas_left        = cgtub::Canvas(window, cgtub::Extent{.x = 0.00f, .y = 0.f, .width = 0.248f, .height = 1.f});
-    cgtub::Canvas canvas_middle_view = cgtub::Canvas(window, cgtub::Extent{.x = 0.25f, .y = 0.f, .width = 0.248f, .height = 1.f});
-    cgtub::Canvas canvas_middle_clip = cgtub::Canvas(window, cgtub::Extent{.x = 0.50f, .y = 0.f, .width = 0.248f, .height = 1.f});
-    cgtub::Canvas canvas_right       = cgtub::Canvas(window, cgtub::Extent{.x = 0.75f, .y = 0.f, .width = 0.25f, .height = 1.f});
+    cgtub::Canvas canvas_left(window, {0.00f, 0.f, 0.248f, 1.f});
+    cgtub::Canvas canvas_middle_view(window, {0.25f, 0.f, 0.248f, 1.f});
+    cgtub::Canvas canvas_middle_clip(window, {0.50f, 0.f, 0.248f, 1.f});
+    cgtub::Canvas canvas_right(window, {0.75f, 0.f, 0.25f, 1.f});
 
     cgtub::SimpleRenderer renderer_left(canvas_left);
     cgtub::SimpleRenderer renderer_middle_view(canvas_middle_view);
     cgtub::SimpleRenderer renderer_middle_clip(canvas_middle_clip);
     cgtub::NDCRenderer    renderer_right(canvas_right);
 
-    // --- WORLD AXES ---
+    glm::vec3 view_box[] = {
+        {-1, -1, -1}, {1, -1, -1}, {-1, 1, -1}, {1, 1, -1}, {-1, -1, 1}, {1, -1, 1}, {-1, 1, 1}, {1, 1, 1}};
+
+    glm::vec3 box_lines[] = {
+        // Bottom
+        view_box[0],
+        view_box[1],
+        view_box[1],
+        view_box[3],
+        view_box[3],
+        view_box[2],
+        view_box[2],
+        view_box[0],
+        // Top
+        view_box[4],
+        view_box[5],
+        view_box[5],
+        view_box[7],
+        view_box[7],
+        view_box[6],
+        view_box[6],
+        view_box[4],
+        // Vertical
+        view_box[0],
+        view_box[4],
+        view_box[1],
+        view_box[5],
+        view_box[2],
+        view_box[6],
+        view_box[3],
+        view_box[7],
+    };
+
+    std::vector<glm::vec3> box_colors(12, glm::vec3(0, 0, 0));
+
     glm::vec3 coordinate_axes_start_end[] = {
-        glm::vec3(0, 0, 0), glm::vec3(1, 0, 0),
-        glm::vec3(0, 0, 0), glm::vec3(0, 1, 0),
-        glm::vec3(0, 0, 0), glm::vec3(0, 0, 1)};
-    glm::vec3 coordinate_axes_color[] = {
-        glm::vec3(1, 0, 0), glm::vec3(0, 1, 0), glm::vec3(0, 0, 1)};
+        {0, 0, 0},
+        {1, 0, 0}, // x
+        {0, 0, 0},
+        {0, 1, 0}, // y
+        {0, 0, 0},
+        {0, 0, 1}, // z
+    };
+    glm::vec3 coordinate_axes_color[] = {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
 
     std::vector<glm::vec3>    bunny_vertices;
     std::vector<glm::u32vec3> bunny_indices;
     ex2::create_bunny_geometry(&bunny_vertices, &bunny_indices);
+    glm::vec3 bunny_color(0.75);
 
-    glm::vec3 bunny_color(0.75f);
-
-    // Sphere for camera visualization
     std::vector<glm::vec3>    sphere_vertices;
     std::vector<glm::u32vec3> sphere_indices;
     cgtub::create_sphere_geometry(0.03f, &sphere_vertices, &sphere_indices);
 
-    float azimuth = 0.f;
-    float fov     = 60.f;
-    float size    = 0.8f;
-    float znear   = 0.35f;
-    float zfar    = 1.60f;
-
+    // State
+    float                   azimuth             = 0.f;
+    float                   fov                 = 60.f;
+    float                   size                = 0.8f;
+    float                   znear               = 0.35f;
+    float                   zfar                = 1.60f;
     ex2::TransformationType transformation_type = ex2::TransformationType::Orthographic;
 
     float time = static_cast<float>(glfwGetTime());
 
+    // --- MAIN LOOP ---
     while (!glfwWindowShouldClose(window))
     {
         cgtub::begin_frame(window);
@@ -84,7 +119,6 @@ int main(int argc, char** argv)
         time      = now;
 
         dispatcher->poll_window_events();
-
         canvas_left.update(dt, dispatcher);
         canvas_middle_view.update(dt, dispatcher);
         canvas_middle_clip.update(dt, dispatcher);
@@ -95,65 +129,138 @@ int main(int argc, char** argv)
 
         ex2::gui(&azimuth, &fov, &size, &znear, &zfar, &transformation_type);
 
-        // --- CAMERA POSITION ---
+        //  view Matrix Calculation
         glm::vec3 camera_origin = glm::vec3(
             std::sin(azimuth) * std::sin(glm::quarter_pi<float>()),
             std::cos(glm::quarter_pi<float>()),
             std::cos(azimuth) * std::sin(glm::quarter_pi<float>()));
 
+        glm::vec3 target   = glm::vec3(0.0f);
+        glm::vec3 v_dir    = glm::normalize(target - camera_origin);
+        glm::vec3 world_up = glm::vec3(0, 1.0f, 0);
+        glm::vec3 v_right  = glm::normalize(glm::cross(v_dir, world_up));
+        glm::vec3 v_up     = glm::cross(v_right, v_dir);
+
+        glm::mat4 R(1.0f);
+        R[0] = glm::vec4(v_right.x, v_up.x, -v_dir.x, 0.0f);
+        R[1] = glm::vec4(v_right.y, v_up.y, -v_dir.y, 0.0f);
+        R[2] = glm::vec4(v_right.z, v_up.z, -v_dir.z, 0.0f);
+
+        glm::mat4 T(1.0f);
+        T[3] = glm::vec4(-camera_origin, 1.0f);
+
+        glm::mat4 LookAt_Matrix = R * T;
+
+        // Projection Matrix Calculation
+        float r = size / 2.0f;
+        float l = -size / 2.0f;
+        float t = size / 2.0f;
+        float b = -size / 2.0f;
+        float n = znear;
+        float f = zfar;
+
+        // Perspective Variables
+        float fov_rad      = glm::radians(fov);
+        float tan_half_fov = std::tan(fov_rad / 2.0f);
+        float S            = 1.0f / tan_half_fov;
+        float A            = -(f + n) / (f - n);
+        float B            = -(2.0f * f * n) / (f - n);
+
+        glm::mat4 Projection_Matrix(1.0f);
+
+        if (transformation_type == ex2::TransformationType::Orthographic)
+        {
+            Projection_Matrix    = glm::mat4(1.0f);
+            Projection_Matrix[0] = glm::vec4(2 / (r - l), 0.0f, 0.0f, 0.0f);
+            Projection_Matrix[1] = glm::vec4(0.0f, 2 / (t - b), 0.0f, 0.0f);
+            Projection_Matrix[2] = glm::vec4(0.0f, 0.0f, -2 / (f - n), 0.0f);
+            Projection_Matrix[3] = glm::vec4(-(r + l) / (r - l), -(t + b) / (t - b), -(f + n) / (f - n), 1.0f);
+        }
+        else // Perspective
+        {
+            Projection_Matrix[0][0] = S;     // Scale X
+            Projection_Matrix[1][1] = S;     // Scale Y
+            Projection_Matrix[2][2] = A;     // Remap Z
+            Projection_Matrix[2][3] = -1.0f; // Perspective divide preparation (w = -z)
+            Projection_Matrix[3][2] = B;     // Z Offset
+            Projection_Matrix[3][3] = 0.0f;  // w = 0
+        }
+
         cgtub::clear(window, 0.f, 0.f, 0.f, 1.f);
+
         canvas_left.clear(glm::vec3(1.f));
-
-        // WORLD AXES
         renderer_left.render_lines(coordinate_axes_start_end, coordinate_axes_color);
-
-        // WORLD MESH
         renderer_left.render_mesh(bunny_vertices, bunny_indices, bunny_color);
 
-        // --- BUILD VIEW MATRIX FIRST ---
-        glm::vec3 world_camera_position = camera_origin;
-        glm::vec3 world_target          = glm::vec3(0.f);
-        glm::vec3 world_up(0.f, 1.f, 0.f);
-
-        glm::vec3 forward  = glm::normalize(world_target - world_camera_position);
-        glm::vec3 backward = -forward;
-        glm::vec3 right    = glm::normalize(glm::cross(world_up, backward));
-        glm::vec3 up       = glm::cross(backward, right);
-
-        glm::mat4 view(1.f);
-
-        // Die Matrix-Zuweisung war KORREKT!
-        // view[0][0] -> Spalte 0, Zeile 0 (r.x)
-        // view[0][1] -> Spalte 0, Zeile 1 (u.x)
-        // Wir bauen die Matrix Zeile für Zeile auf (was in memory den Spalten entspricht)
-        view[0][0] = right.x;
-        view[0][1] = up.x;
-        view[0][2] = backward.x;
-
-        view[1][0] = right.y;
-        view[1][1] = up.y;
-        view[1][2] = backward.y;
-
-        view[2][0] = right.z;
-        view[2][1] = up.z;
-        view[2][2] = backward.z;
-
-        view[3][0] = -glm::dot(right, world_camera_position);
-        view[3][1] = -glm::dot(up, world_camera_position);
-        view[3][2] = -glm::dot(backward, world_camera_position);
-        view[3][3] = 1.f;
-
-        // --- RENDER CAMERA AXES ---
-        ex2::render_camera(renderer_left, view, glm::mat4(1.f));
-
-        // --- RENDER CAMERA SPHERE ---
-        std::vector<glm::vec3> cam_vertices = sphere_vertices;
-        for (auto& v : cam_vertices)
+        std::vector<glm::vec3> cam_sphere = sphere_vertices;
+        for (auto& v : cam_sphere)
             v += camera_origin;
+        renderer_left.render_mesh(cam_sphere, sphere_indices, glm::vec3(1, 0, 1));
 
-        renderer_left.render_mesh(cam_vertices, sphere_indices, glm::vec3(1, 0, 1));
+        ex2::render_camera(renderer_left, LookAt_Matrix, Projection_Matrix);
 
-        // END FRAME
+        canvas_middle_view.clear(glm::vec3(1.f));
+
+        std::vector<glm::vec3> bunny_view;
+        bunny_view.reserve(bunny_vertices.size());
+        for (const auto& v : bunny_vertices)
+        {
+            bunny_view.push_back(glm::vec3(LookAt_Matrix * glm::vec4(v, 1.0f)));
+        }
+
+        std::vector<glm::vec3> axes_view;
+        for (const auto& v : coordinate_axes_start_end)
+        {
+            axes_view.push_back(glm::vec3(LookAt_Matrix * glm::vec4(v, 1.0f)));
+        }
+
+        renderer_middle_view.render_mesh(bunny_view, bunny_indices, bunny_color);
+        renderer_middle_view.render_lines(axes_view, coordinate_axes_color);
+        ex2::render_camera(renderer_middle_view, glm::mat4(1.0f), Projection_Matrix);
+
+        canvas_middle_clip.clear(glm::vec3(1.0f));
+
+        std::vector<glm::vec3> bunny_ndc_visual_vec3;
+        std::vector<glm::vec4> bunny_ndc_real_vec4;
+
+        bunny_ndc_visual_vec3.reserve(bunny_vertices.size());
+        bunny_ndc_real_vec4.reserve(bunny_vertices.size());
+
+        for (const auto& v : bunny_view)
+        {
+            glm::vec4 p = Projection_Matrix * glm::vec4(v, 1.0f);
+
+            bunny_ndc_real_vec4.push_back(p);
+
+            if (p.w != 0.0f)
+                bunny_ndc_visual_vec3.push_back(glm::vec3(p) / p.w);
+            else
+                bunny_ndc_visual_vec3.push_back(glm::vec3(p));
+        }
+
+        renderer_middle_clip.render_mesh(bunny_ndc_visual_vec3, bunny_indices, bunny_color);
+        renderer_middle_clip.render_lines(box_lines, box_colors);
+
+        std::vector<glm::vec3> ndc_axes_visual;
+        std::vector<glm::vec4> ndc_axes_real;
+
+        for (const auto& v : axes_view)
+        {
+            glm::vec4 p = Projection_Matrix * glm::vec4(v, 1.0f);
+            ndc_axes_real.push_back(p);
+
+            if (p.w != 0.0f)
+                ndc_axes_visual.push_back(glm::vec3(p) / p.w);
+            else
+                ndc_axes_visual.push_back(glm::vec3(p));
+        }
+        renderer_middle_clip.render_lines(ndc_axes_visual, coordinate_axes_color);
+
+        canvas_right.clear(glm::vec3(1.0f));
+
+        renderer_right.render_mesh(bunny_ndc_real_vec4, bunny_indices, bunny_color);
+        renderer_right.render_lines(ndc_axes_real, coordinate_axes_color);
+
         cgtub::end_frame(window);
     }
 
